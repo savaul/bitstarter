91b75b6b09d3cee0bbfa80830812ce8d58cd22af
Mihai Sava <savaul@gmail.com>
2013-07-15 04:24:38 -0700

    update heroku

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
+var buf = new Buffer ("test buffer", "utf8");
 var app = express.createServer(express.logger());
-
+buf = fs.readFileSync("index.html", "utf8");
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log(buf.toString('utf8'));
 });
